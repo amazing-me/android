800e78f85fc5b4ac231c776397da183efd4620f2
Zachary Halaby <-bash>
2017-08-29 01:16:32 -0400

    adding some baseline model classes to build from

---
+++ AmazingMeSample/app/src/main/java/amazingme/database/FireBase.java
@@ -0,0 +1,11 @@
+package amazingme.database;
+
+/**
+ * Created by zacharyhalaby on 8/29/17.
+ */
+
+public class FireBase {
+
+    //gonna keep this so we can more easily communicate with the firebase
+
+}
---
+++ AmazingMeSample/app/src/main/java/amazingme/model/Game.java
@@ -0,0 +1,13 @@
+package amazingme.model;
+
+/**
+ * Created by zacharyhalaby on 8/29/17.
+ */
+
+interface Game {
+
+    void takeover();
+
+    int resign();
+
+}
