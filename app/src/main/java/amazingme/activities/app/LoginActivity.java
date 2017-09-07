53887822e10cbd1962e8042977191445fb1fad91
DKim0509 <dkim5994@gmail.com>
2017-09-07 17:49:27 -0400

    forgot password text button added

--- app/src/main/java/amazingme/activities/app/LoginActivity.java
+++ app/src/main/java/amazingme/activities/app/LoginActivity.java
@@ -2,17 +2,12 @@ package amazingme.activities.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.amazingme.activities.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 import amazingme.app.EnumeratedActivity;
 import amazingme.database.FirebaseHelper;
@@ -21,6 +16,8 @@ import amazingme.model.AmazingMeAppCompatActivity;
 public class LoginActivity extends AmazingMeAppCompatActivity {
     private EditText emailEditText, passwordEditText;
     private Button loginBtn, registerBtn;
+    private TextView forgotpasswordTextBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +28,7 @@ public class LoginActivity extends AmazingMeAppCompatActivity {
         passwordEditText = (EditText) findViewById(R.id.passwordEditText);
         loginBtn = (Button) findViewById(R.id.loginBtn);
         registerBtn = (Button) findViewById(R.id.registerBtn);
+        forgotpasswordTextBtn = (TextView) findViewById(R.id.forgotpasswordBtn);
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +47,17 @@ public class LoginActivity extends AmazingMeAppCompatActivity {
                 FirebaseHelper.loginUser(email, password, getApplicationContext());
             }
         });
+
+        forgotpasswordTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent registerIntent = new Intent(getApplicationContext(), ForgotPasswordActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
+
+
 
     @Override
     public EnumeratedActivity activityName() {
