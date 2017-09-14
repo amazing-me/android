fddd3beeb5975e3f3811ae85319b75c14d5ae5d6
Zachary Halaby <-bash>
2017-09-13 23:51:13 -0400

    one minor change in the Game super class to make it use what it needs to

--- app/src/main/java/amazingme/model/AmazingMeGame.java
+++ app/src/main/java/amazingme/model/AmazingMeGame.java
@@ -1,9 +1,7 @@
 package amazingme.model;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import amazingme.activities.app.MainMenu;
+import amazingme.app.EnumeratedActivity;
 
 public abstract class AmazingMeGame extends AmazingMeAppCompatActivity {
 
@@ -16,9 +14,7 @@ public abstract class AmazingMeGame extends AmazingMeAppCompatActivity {
         } else {
             gameResultsBundle.putSerializable("GAME RESULTS BUNDLE", new GameResult(0, new Problem[] {Problem.DID_NOT_FINISH}));
         }
-        //ActivityManager.getInstance().goTo(EnumeratedActivity.MAIN_MENU, gameResultsBundle);
-        Intent backToGameSelection = new Intent(getApplicationContext(), MainMenu.class); //this should be the game screen glass later
-        startActivity(backToGameSelection); // this is technically resuming, but i don't believe it's important.
+        goTo(EnumeratedActivity.MAIN_MENU);
     }
 
     public abstract GameResult gameResults();
