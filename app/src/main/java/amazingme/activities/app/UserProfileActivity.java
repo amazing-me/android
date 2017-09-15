4f8bc0777e41e86da0f6c6504670dbf934801efb
vbuck3 <vbuck3@gatech.edu>
2017-09-14 21:28:37 -0400

    Vernon update

--- app/src/main/java/amazingme/activities/app/UserProfileActivity.java
+++ app/src/main/java/amazingme/activities/app/UserProfileActivity.java
@@ -17,11 +17,36 @@ public class UserProfileActivity extends AmazingMeAppCompatActivity {
     private Button backBtn;
 
     @Override
+<<<<<<< Updated upstream
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
         backBtn = (Button) findViewById(backToRegisterBtn);
+=======
+    public EnumeratedActivity activityName() {
+        return EnumeratedActivity.USERPROFILE;
+    }
+    //git stash
+    @Override
+    public void bindToUserInterface() {
+        backBtn = (Button) findViewById(R.id.user_profile_back_button);
+        doneBtn = (Button) findViewById(R.id.button3);
+        childBtn = (Button) findViewById(R.id.button2);
+        firstnameEditText = (EditText) findViewById(R.id.editText2);
+        lastnameEditText = (EditText) findViewById(R.id.editText4);
+        ageEditText = (EditText) findViewById(R.id.editText5);
+        sexEditText = (EditText) findViewById(R.id.editText7);
+        profview = (TextView) findViewById(R.id.textView4);
+        abilityview = (TextView) findViewById(R.id.textView5);
+
+        yesnoSpinner = (Spinner) findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> spinAdapter = new ArrayAdapter<String>(UserProfileActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.yesno));
+        spinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        yesnoSpinner.setAdapter(spinAdapter);
+>>>>>>> Stashed changes
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
