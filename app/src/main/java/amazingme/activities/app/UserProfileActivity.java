f6c8294e9d1d3aaeed8b37f406dae34b854cb58f
vbuck3 <vbuck3@gatech.edu>
2017-09-14 22:42:58 -0400

    drop down addition

--- app/src/main/java/amazingme/activities/app/UserProfileActivity.java
+++ app/src/main/java/amazingme/activities/app/UserProfileActivity.java
@@ -17,7 +17,7 @@ public class UserProfileActivity extends AmazingMeAppCompatActivity {
     private Button backBtn, doneBtn, childBtn;
     private EditText firstnameEditText, lastnameEditText, ageEditText, sexEditText;
     private TextView profview,abilityview;
-    private Spinner yesnoSpinner;
+    private Spinner yesnoSpinner, malefemale;
 
     public UserProfileActivity() { super(R.layout.activity_user_profile); }
 
@@ -34,16 +34,23 @@ public class UserProfileActivity extends AmazingMeAppCompatActivity {
         firstnameEditText = (EditText) findViewById(R.id.editText2);
         lastnameEditText = (EditText) findViewById(R.id.editText4);
         ageEditText = (EditText) findViewById(R.id.editText5);
-        sexEditText = (EditText) findViewById(R.id.editText7);
+        //sexEditText = (EditText) findViewById(R.id.editText7);
         profview = (TextView) findViewById(R.id.textView4);
         abilityview = (TextView) findViewById(R.id.textView5);
 
         yesnoSpinner = (Spinner) findViewById(R.id.spinner1);
+        malefemale = (Spinner) findViewById(R.id.spinner2);
 
         ArrayAdapter<String> spinAdapter = new ArrayAdapter<String>(UserProfileActivity.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.yesno));
         spinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         yesnoSpinner.setAdapter(spinAdapter);
+
+        ArrayAdapter<String> spinAdapter2 = new ArrayAdapter<String>(UserProfileActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.sex));
+        spinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        malefemale.setAdapter(spinAdapter2);
+
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
