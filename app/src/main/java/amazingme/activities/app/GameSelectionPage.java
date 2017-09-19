30508ac9250c679f8a6ecc1f15434dfcbd8ccf97
DKim0509 <dkim5994@gmail.com>
2017-09-19 17:27:06 -0400

    created game selection page activity

---
+++ app/src/main/java/amazingme/activities/app/GameSelectionPage.java
@@ -0,0 +1,15 @@
+package amazingme.activities.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amazingme.activities.R;
+
+public class GameSelectionPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_selection_page);
+    }
+}
